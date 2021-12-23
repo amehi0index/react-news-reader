6aa70e8330868fa4876c1ac7244bab0c29a293d3
unknown <amehi4yus@gmail.com>
2021-12-23 11:50:24 -0800

    Disable scrolling on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import InfoCards from '../components/infocards/InfoCards'
 import NewsCards from '../components/newscards/NewsCards'
 
 
 const Home = ({ articles, activeArticle }) => {
+
+    useEffect(() => {
+        if(!articles.length){
+            document.body.style.overflow = "hidden"
+        }else{
+            document.body.style.overflowY = "scroll"
+        }
+    }, [articles.length])
+
+
     if(!articles.length){
         return (
             <div className="page-container">
